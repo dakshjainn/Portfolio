aaa4d7e5f912cd97d6ae9fd26e13d9aeb468f7e0
dakshjainn <dakshjainn2004@gmail.com>
2025-01-23 19:28:09 +0530

    chess project icon changed

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,13 +1,14 @@
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
+import chessIcon from "../../assets/chessIcon.jpg";
 import ProjectCard from "../../common/ProjectCard";
+
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.ProjectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={chessIcon}
           link={"https://github.com/dakshjainn/multiplayer_chess"}
           h3={"Multiplayer Chess"}
           p={"Play chess with your friends"}
